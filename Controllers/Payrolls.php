79a49a31d043c391bba0b9692e4476293723fcac
dgmshell <dgmaldonado.dev@gmail.com>
2025-03-05 20:52:04 -0600

    Insider update

--- Controllers/Payrolls.php
+++ Controllers/Payrolls.php
@@ -33,6 +33,8 @@ class Payrolls extends Controllers
             $this->views->getViews($this, 'payrolls', $data,$payrolls);
 
 
+
+
     }
 
     /**
--- Views/Template/pageHeader.php
+++ Views/Template/pageHeader.php
@@ -1,29 +1,12 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-    <title>Arcade - Architecture</title>
     <meta charset="UTF-8">
-    <meta name="description" content="Arcade - Architecture Template">
-    <meta name="keywords" content="arcade, architecture, onepage, creative, html">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Favicon -->
-    <link href="img/favicon.ico" rel="shortcut icon"/>
-
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
-
-    <!-- Stylesheets -->
-    <link rel="stylesheet" href="<?php echo files(); ?>src/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="<?php echo files(); ?>src/css/font-awesome.min.css"/>
-    <link rel="stylesheet" href="<?php echo files(); ?>src/css/animate.css"/>
-    <link rel="stylesheet" href="<?php echo files(); ?>src/css/owl.carousel.css"/>
-    <link rel="stylesheet" href="<?php echo files(); ?>src/css/style.css"/>
-
-
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Trebol</title>
+    <link rel="stylesheet" href="<?php echo files(); ?>css/libraries/toast.css">
+    <script>const router = "<?php echo router(); ?>"</script>
 </head>
 <body>
